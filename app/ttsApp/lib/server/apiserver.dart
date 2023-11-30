b6012950e162063def4b7a558cc756b6a7cb771d
이정연 <99dlwjddus@naver.com>
2023-11-30 23:22:13 +0900

    AWS EC2 배포 서버 주소 변경

--- app/ttsApp/lib/server/apiserver.dart
+++ app/ttsApp/lib/server/apiserver.dart
@@ -1,6 +1,6 @@
 class ApiServer {
 
-  final apiserver = 'http://3.36.86.193:5000';
+  final apiserver = 'http://3.36.100.141:5000';
   // final apiserver = "http://192.168.20.166:5000";
 
   String getApiServer() {
